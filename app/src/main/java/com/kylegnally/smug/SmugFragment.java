747d9e614c5e61f4b0e689bd268ea1fabf9037a5
Kyle Nally <kylegnally@gmail.com>
2018-06-20 09:25:01 -0400

    Change to ConstraintLayout. Change image name in code. Add to strings.xml.

--- app/src/main/java/com/kylegnally/smug/SmugFragment.java
+++ app/src/main/java/com/kylegnally/smug/SmugFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class SmugFragment extends Fragment {
@@ -21,7 +22,7 @@ public class SmugFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_smug, container, false);
 
-        LinearLayout linearLayout = view.findViewById(R.id.linear_layout);
+        ImageView imageView = (ImageView) view.findViewById(R.id.ericCouch);
 
         return view;
     }
