0bfe5da0ff28e33ff5cdfb4568ea6151bc9cd383
Kyle Nally <kylegnally@gmail.com>
2018-06-20 08:09:57 -0400

    Add SmugFragment.

--- app/src/main/java/com/kylegnally/smug/SmugActivity.java
+++ app/src/main/java/com/kylegnally/smug/SmugActivity.java
@@ -1,6 +1,8 @@
 package com.kylegnally.smug;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +12,13 @@ public class SmugActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_smug);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null){
+            fragment = new SmugFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
     }
 }
