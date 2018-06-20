582e9ff3dec6336cbf426089a6099281a9ca3293
Kyle Nally <kylegnally@gmail.com>
2018-06-20 09:05:38 -0400

    Create Smug.java. Create references to Views within SingleFragmentActivity.java. Remove redundant cast.

---
+++ app/src/main/java/com/kylegnally/smug/Smug.java
@@ -0,0 +1,4 @@
+package com.kylegnally.smug;
+
+public class Smug {
+}
--- app/src/main/java/com/kylegnally/smug/SmugFragment.java
+++ app/src/main/java/com/kylegnally/smug/SmugFragment.java
@@ -1,7 +1,28 @@
 package com.kylegnally.smug;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class SmugFragment extends Fragment {
+    private Smug mSmug;
 
+    public static SmugFragment newInstance() {
+        return new SmugFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_smug, container, false);
+
+        LinearLayout linearLayout = view.findViewById(R.id.linear_layout);
+
+        return view;
+    }
 }
