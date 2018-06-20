13369b949e6f94667c4202c9a318c28c77914879
Kyle Nally <kylegnally@gmail.com>
2018-06-20 07:18:18 -0400

    Initial commit.

---
+++ app/src/main/java/com/kylegnally/smug/SmugActivity.java
@@ -0,0 +1,14 @@
+package com.kylegnally.smug;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SmugActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smug);
+    }
+}
