b5cf8ab882d8f412d8568a7c3d1e4a66b353aa98
Kyle Nally <kylegnally@gmail.com>
2018-06-22 16:05:55 -0400

    Revert changes.

--- app/src/main/java/com/kylegnally/smug/SmugWidget.java
+++ app/src/main/java/com/kylegnally/smug/SmugWidget.java
@@ -13,10 +13,10 @@ public class SmugWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.smug_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        // views.setTextViewText(R.id.appwidget_text, widgetText);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
